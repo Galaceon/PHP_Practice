39bd3fc9d0cf50cd400f590326dbde9cfc3192d4
Galaceon <antoniogarcialtr@gmail.com>
2025-04-30 21:28:08 +0200

    01 helloWorld finished

--- 01-hola-mundo.php
+++ 01-hola-mundo.php
@@ -1,1 +1,11 @@
-<h1> Hola Mundo </h1>
+<?php include 'includes/header.php';
+
+// echo "Hola mundo";
+// echo "<h1>Hola mundo</h1>";
+// print "hola mundo";
+
+// var_dump("Hola mundo y tal");
+
+// var_dump(20);
+
+include 'includes/footer.php';
