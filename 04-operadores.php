0a1476ef3aa68c78c2ae45461ee283566179d527
Galaceon <antoniogarcialtr@gmail.com>
2025-05-02 01:47:19 +0200

    04-operadores

--- 04-operadores.php
+++ 04-operadores.php
@@ -1,6 +1,37 @@
 <?php include 'includes/header.php';
 
+$numero1 = 10;
+$numero2 = 20;
 
+//Sumar
+echo $numero1 + $numero2;
+
+//Restar
+echo $numero1 - $numero2;
+
+//Multiplicar
+echo $numero1 * $numero2;
+
+//Dividir
+echo $numero1 / $numero2;
+
+//Multiplicar cierta cantiedad de veces mismo numero
+echo 2 ** 3; // 2 x 2 = 4 x 2 = 8
+
+//Modulo
+echo $numero1 % $numero2;
+
+//Incrementar
+$numero1++;
+
+//Decrementar
+$numero2--;
+
+//Incrementar en 10
+$numero1 += 10;
+
+//Decrementar en 10
+$numero2 -= 10;
 
 
 include 'includes/footer.php';
