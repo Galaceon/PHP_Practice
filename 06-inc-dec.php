91ff0711d0fba0a5fcbd8f02553871ae517f5a57
Galaceon <antoniogarcialtr@gmail.com>
2025-05-02 02:45:35 +0200

    06 incremento y decremento

--- 06-inc-dec.php
+++ 06-inc-dec.php
@@ -1,6 +1,16 @@
 <?php include 'includes/header.php';
 
+//Incrementar
+$numero1++;
 
+//Decrementar
+$numero2--;
+
+//Incrementar en 10
+$numero1 += 10;
+
+//Decrementar en 10
+$numero2 -= 10;
 
 
 include 'includes/footer.php';
