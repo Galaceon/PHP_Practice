5becfbc3d4e7177ac3dcceef6cd18fcbb2f91a77
Galaceon <antoniogarcialtr@gmail.com>
2025-05-03 05:16:20 +0200

    09-Arrays Asociativos

--- 09-arrays-assoc.php
+++ 09-arrays-assoc.php
@@ -1,6 +1,33 @@
 <?php include 'includes/header.php';
 
+$cliente = [
+    'nombre' => 'Antonio',
+    'saldo' => 200,
+    'informacion' => [
+        'tipo' => 'premium'
+    ]
+];
+// Mostrar el array asociativo
+echo "<pre>";
+var_dump($cliente);
+echo "</pre>";
+
+// Añadir un nuevo elemento al array asociativo
+$cliente['codigo'] = 123456;
+echo "<pre>";
+var_dump($cliente);
+echo "</pre>";
+
+// Modificar un elemento del array asociativo
+$cliente['saldo'] = 1300;
+echo "<pre>";
+var_dump($cliente);
+echo "</pre>";
 
 
+// Mostrar informacion de cualquier punto del array
+echo "<pre>";
+var_dump($cliente['informacion']['tipo']);
+echo "</pre>";
 
 include 'includes/footer.php';
