43851150b7cac1bad2f3685a036920a3fb1a9e8b
Galaceon <antoniogarcialtr@gmail.com>
2025-05-03 02:18:15 +0200

    08-arrays and some arrayMethods

--- 08-arrays.php
+++ 08-arrays.php
@@ -1,6 +1,23 @@
 <?php include 'includes/header.php';
 
+$carrito = ['Tablet', 'Television', 'Ordenador'];
 
 
+// Util para ver los contenidor de un array - MUY IMPORTANTE
+echo "<pre>";
+var_dump($carrito);
+echo "</pre>";
+
+// Acceder a un elemento de array
+echo $carrito[1];
+
+// Añadir un elemento en el indice deseado
+$carrito[3] = 'pc';
+
+// Añadir elemento nuevo al final
+array_push($carrito, 'raton');
+
+// Añadir elemento nuevo al inicio
+array_unshift($carrito, 'teclado');
 
 include 'includes/footer.php';
