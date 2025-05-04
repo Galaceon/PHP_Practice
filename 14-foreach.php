8750183ddd2d64cc9ad2437c9567e8504669016d
Galaceon <antoniogarcialtr@gmail.com>
2025-05-05 00:09:27 +0200

    14-foreach y operador ternario

--- 14-foreach.php
+++ 14-foreach.php
@@ -1,6 +1,31 @@
 <?php include 'includes/header.php';
 
+$productos = [
+    [
+        'nombre' => 'Camiseta',
+        'precio' => 300,
+        'disponible' => true
+    ],
+    [
+        'nombre' => 'Monitor',
+        'precio' => 400,
+        'disponible' => false
+    ],
+    [
+        'nombre' => 'Teclado',
+        'precio' => 100,
+        'disponible' => true
+    ]
+];
 
+foreach ($productos as $producto) { ?>
+    <li>
+        <p>Producto: <?php echo $producto['nombre']; ?> </p>
+        <p>Precio: <?php echo $producto['precio']; ?> </p>
+        <p><?php echo ($producto['disponible']) ? 'Disponible' : 'No disponible'; ?> </p>
+    </li>
+    <?php
+}
 
 
 include 'includes/footer.php';
