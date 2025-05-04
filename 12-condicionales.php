4e6a5a938721fa57155ee7b9f3199e08cfc08bda
Galaceon <antoniogarcialtr@gmail.com>
2025-05-04 02:55:05 +0200

    12-condicionales

--- 12-condicionales.php
+++ 12-condicionales.php
@@ -1,7 +1,56 @@
 <?php include 'includes/header.php';
 
+$autenticado = true;
+$admin = false;
+
+if($autenticado || $admin) {
+    echo "Usuario autenticado correctamente";
+} else {
+    echo 'Usuario no autenticado';
+}
+
+// if anidados
+$cliente = [
+    'nombre' => 'Juan',
+    'saldo' => 200,
+    'informacion' => [
+        'tipo' => 'premium',
+        'fecha_registro' => '2023-10-01'
+    ]
+];
+if (!empty($cliente)) {
+    echo 'El cliente existe';
+
+    if ($cliente['saldo'] > 0) {
+        echo 'El cliente tiene saldo';
+    } else {
+        echo 'El cliente no tiene saldo';
+    }
+}
+
+// else if
+if($cliente['saldo'] > 0) {
+    echo 'El cliente tiene saldo';
+} else if($cliente['informacion']['tipo'] === 'premium') {
+    echo 'El cliente es premium';
+} else {
+    echo 'El cliente no tiene saldo y no es premium';
+}
 
 
+// switch
+$tecnologia = 'PHP';
 
+switch ($tecnologia) {
+    case 'PHP':
+        echo 'El lenguaje es PHP';
+        break;
+    case 'JavaScript':
+        echo 'El lenguaje es JavaScript';
+        break;
+    default:
+        echo 'Lenguaje no reconocido';
+        break;
+}
 
 include 'includes/footer.php';
