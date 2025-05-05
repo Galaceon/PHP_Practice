9b9350252b4f826b1543b33d236acaa7ae2e2ae8
Galaceon <antoniogarcialtr@gmail.com>
2025-05-05 05:51:29 +0200

    18 json, Final practice

--- 18.json.php
+++ 18.json.php
@@ -1,8 +1,31 @@
 <?php include 'includes/header.php';
 
+$productos = [
+    [
+        'nombre' => 'Camiseta',
+        'precio' => 300,
+        'disponible' => true
+    ],
+    [
+        'nombre' => 'Monitor',
+        'precio' => 400,
+        'disponible' => false
+    ],
+    [
+        'nombre' => 'Teclado',
+        'precio' => 100,
+        'disponible' => true
+    ]
+];
+
+echo "<pre>";
+$json = json_encode($productos, JSON_UNESCAPED_UNICODE); //convertir a string
+
+$json_array = json_decode($json); //convertir a array
 
 
+var_dump($json); //ver el string
+var_dump($json_array); //ver el array
 
-
-
+echo "</pre>";
 include 'includes/footer.php';
